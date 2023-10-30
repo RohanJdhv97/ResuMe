025db5c6e5a84b29ccb6676caeb53bffb3fde9de
RohanJdhv97 <rohan.jadhav@zysk.tech>
2023-10-30 19:56:12 +0530

    UPDATE: placed the code of export for the Candidate resource

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -7,6 +7,9 @@ use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListUsers extends ListRecords
 {
@@ -21,6 +24,24 @@ class ListUsers extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromForm()
+                        ->withFilename('Candidate')
+                        ->only([
+                            'first_name', 'last_name', 'email','phone','role_id'
+                        ])
+                        ->withColumns([
+                            Column::make('first_name')->heading('first_name'),
+                            Column::make('last_name')->heading('last_name'),
+                            Column::make('email')->heading('email'),
+                            Column::make('phone')->heading('phone'),
+                            Column::make('role_id')->heading('role_id'),
+                        ])
+                        ->askForFilename()
+                        ->modifyQueryUsing(fn ($query) => $query->where('role_id', 3)),
+                ]) 
         ];
     }
 }
