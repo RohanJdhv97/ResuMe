be2611e592cf238eba8e2fa8dded59c93058cdd1
RohanJdhv97 <rohan.jadhav@zysk.tech>
2023-10-30 19:55:18 +0530

    UPDATE:placed the export option for the interviewer resource

--- app/Filament/Resources/InterviewerResource/Pages/ListInterviewers.php
+++ app/Filament/Resources/InterviewerResource/Pages/ListInterviewers.php
@@ -7,6 +7,9 @@ use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
+use pxlrbt\FilamentExcel\Columns\Column;
 
 class ListInterviewers extends ListRecords
 {
@@ -22,7 +25,25 @@ class ListInterviewers extends ListRecords
         return [
             Actions\CreateAction::make(),
             ExcelImportAction::make()
-            ->color('primary')
+            ->color('primary'),
+            ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromForm()
+                        ->withFilename('Interviewer')
+                        ->only([
+                            'first_name', 'last_name', 'email','phone','role_id'
+                        ])
+                        ->withColumns([
+                            Column::make('first_name')->heading('first_name'),
+                            Column::make('last_name')->heading('last_name'),
+                            Column::make('email')->heading('email'),
+                            Column::make('phone')->heading('phone'),
+                            Column::make('role_id')->heading('role_id'),
+                        ])
+                        ->askForFilename()
+                        ->modifyQueryUsing(fn ($query) => $query->where('role_id', 4)),
+                ]) 
         ];
     }
 }
