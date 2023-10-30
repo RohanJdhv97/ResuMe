28b2d60efecef7515d0423de747fa4c86352cd5f
RohanJdhv97 <rohan.jadhav@zysk.tech>
2023-10-30 19:55:42 +0530

    UPDATE: placed the code of export for company skills resource

--- app/Filament/Resources/CompanySkillsResource/Pages/ListCompanySkills.php
+++ app/Filament/Resources/CompanySkillsResource/Pages/ListCompanySkills.php
@@ -6,6 +6,9 @@ use App\Filament\Resources\CompanySkillsResource;
 use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListCompanySkills extends ListRecords
 {
@@ -16,7 +19,22 @@ class ListCompanySkills extends ListRecords
         return [
             Actions\CreateAction::make(),
             ExcelImportAction::make()
-                ->color('primary')
+                ->color('primary'),
+                ExportAction::make()
+                ->exports([
+                    ExcelExport::make()
+                        ->fromForm()
+                        ->withFilename('Company Skills')
+                        ->only([
+                            'name','cutoff','company_id'
+                        ])
+                        ->withColumns([
+                            Column::make('name')->heading('name'),
+                            Column::make('cutoff')->heading('cutoff'),
+                            Column::make('company_id')->heading('company_id'),
+                        ])
+                        ->askForFilename(),
+                ]) 
         ];
     }
 }
